1d772a2f6f00e88d59c2a9677ec08d61d2a89f47
jaytee87 <truong87@yahoo.com>
2017-01-04 01:56:16 -0600

    Build Start
    Just created new Xcode project to learn core data

---
+++ DreamLister/ItemType+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  ItemType+CoreDataClass.swift
+//  DreamLister
+//
+//  Created by Johnny on 1/4/17.
+//  Copyright © 2017 Johnny. All rights reserved.
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+@objc(ItemType)
+public class ItemType: NSManagedObject {
+
+}
